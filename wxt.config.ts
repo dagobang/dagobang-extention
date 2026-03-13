3dfe6019ba9ee26a48340816b9d4638b18126e26
SeasonsRich <0xseasonsrich@gmail.com>
2026-03-13 22:10:31 +0800

    Update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   manifest: {
     name: 'Dagobang 打狗棒',
     description: 'Dagobang non-custodial Meme Trading Tool',
-    version: '0.1.17',
-    permissions: ['storage', 'alarms', 'webRequest', 'webRequestBlocking', 'sidePanel'],
+    version: '0.1.18',
+    permissions: ['storage', 'alarms', 'sidePanel'],
     host_permissions: [
       'https://*.gmgn.ai/*',
       'https://gmgn.ai/*',
